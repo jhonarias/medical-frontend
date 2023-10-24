48492d5974aa04ec7d78097d97a9b4ea96a590d9
John Arias <john.arias@newshore.es>
2023-10-24 16:27:16 -0500

    update domain

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,9 +1,9 @@
 export const protocol = location.protocol;
 
 export const environment = {
-    apiURLAuthentication: 'https://d1oyc4j1qqbn95.cloudfront.net/auth',
-    apiURLTopic: 'https://d1oyc4j1qqbn95.cloudfront.net/topic',
-    apiURLSubtopic: 'https://d1oyc4j1qqbn95.cloudfront.net/subtopic',
-    apiURLQuestion: 'https://d1oyc4j1qqbn95.cloudfront.net/question',
-    apiURLAnswer: 'https://d1oyc4j1qqbn95.cloudfront.net/answer',
+    apiURLAuthentication: 'https://www.api-learn-about-topics.com/auth',
+    apiURLTopic: 'https://www.api-learn-about-topics.com/topic',
+    apiURLSubtopic: 'https://www.api-learn-about-topics.com/subtopic',
+    apiURLQuestion: 'https://www.api-learn-about-topics.com/question',
+    apiURLAnswer: 'https://www.api-learn-about-topics.com/answer',
 }
